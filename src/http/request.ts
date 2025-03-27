c50a254a943463d830f5baba80f2bdb88e37913f
johnnywwy <johnnywwy@gmail.com>
2025-03-27 21:46:31 +0800

    feat: 封装 axios 封装

--- src/http/request.ts
+++ src/http/request.ts
@@ -1,16 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 import type {
   AxiosInstance,
   AxiosRequestConfig,
   AxiosResponse,
   AxiosError,
   InternalAxiosRequestConfig,
-} from 'axios';
+} from "axios";
+import { ElMessage } from "element-plus";
+import { getMessageInfo } from "./status";
+
+interface BaseResponse<T = any> {
+  code: number | string;
+  message: string;
+  data: T;
+  status?: number | string;
+}
 
 const service: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_BASEURL,
-    timeout: 15000,
-  });
+  baseURL: import.meta.env.VITE_APP_API_BASEURL,
+  timeout: 15000,
+});
 
 // axios实例拦截请求
 service.interceptors.request.use(
@@ -25,11 +34,85 @@ service.interceptors.request.use(
 // axios实例拦截响应
 service.interceptors.response.use(
   (response: AxiosResponse) => {
-    return response
+    if (response.status === 200) {
+      return response.data;
+    }
+    ElMessage({
+      message: getMessageInfo(response.status),
+      type: "error",
+    });
   },
+  // 请求失败
   (error: any) => {
-    return Promise.reject(error);
+    const { response } = error;
+    if (response) {
+      ElMessage({
+        message: getMessageInfo(response.status),
+        type: "error",
+      });
+      return Promise.reject(response.data);
+    }
+    ElMessage({
+      message: "网络连接异常,请稍后再试!",
+      type: "error",
+    });
   }
 );
 
-export default service
+// 此处相当于二次响应拦截
+// 为响应数据进行定制化处理
+const requestInstance = <T = any>(config: AxiosRequestConfig): Promise<T> => {
+  const conf = config;
+  return new Promise((resolve, reject) => {
+    service
+      .request<any, AxiosResponse<BaseResponse>>(conf)
+      .then((res: AxiosResponse<BaseResponse>) => {
+        const data = res.data; // 如果data.code为错误代码返回message信息
+        if (data.code != 0) {
+          ElMessage({
+            message: data.message,
+            type: "error",
+          });
+          reject(data.message);
+        } else {
+          ElMessage({
+            message: data.message,
+            type: "success",
+          }); // 此处返回data信息 也就是 api 中配置好的 Response类型
+          resolve(data.data as T);
+        }
+      });
+  });
+};
+
+export function get<T = any, U = any>(
+  config: AxiosRequestConfig,
+  url: string,
+  parms?: U
+): Promise<T> {
+  return requestInstance({ ...config, url, method: "GET", params: parms });
+}
+export function post<T = any, U = any>(
+  config: AxiosRequestConfig,
+  url: string,
+  data: U
+): Promise<T> {
+  return requestInstance({ ...config, url, method: "POST", data: data });
+}
+
+export function put<T = any, U = any>(
+  config: AxiosRequestConfig,
+  url: string,
+  parms?: U
+): Promise<T> {
+  return requestInstance({ ...config, url, method: "PUT", params: parms });
+}
+export function del<T = any, U = any>(
+  config: AxiosRequestConfig,
+  url: string,
+  data: U
+): Promise<T> {
+  return requestInstance({ ...config, url, method: "DELETE", data: data });
+}
+
+export default service;
