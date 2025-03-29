565f986fcf27a41d2cb5ac755d77993b6d48c715
johnnywwy <johnnywwy@gmail.com>
2025-03-29 23:31:22 +0800

    feat: 路由渲染菜单基本完成

--- src/layout/components/menu.vue
+++ src/layout/components/menu.vue
@@ -1,13 +1,25 @@
 <template>
-  <el-menu default-active="/">
-    <el-menu-item index="/"> 项目介绍 </el-menu-item>
-    <el-menu-item index="/user"> 用户列表 </el-menu-item>
+  <el-menu :default-active="activeMenu" router>
+    <el-menu-item v-for="item in menuList" :key="item.path" :index="item.path">
+      {{ item.meta.title }}
+    </el-menu-item>
+    <!-- <el-menu-item index="/user"> 用户列表 </el-menu-item>
     <el-menu-item index="/role"> 角色列表 </el-menu-item>
-    <el-menu-item index="/auth"> 权限列表 </el-menu-item>
+    <el-menu-item index="/auth"> 权限列表 </el-menu-item> -->
   </el-menu>
 </template>
 
 <script setup lang='ts'>
+import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const router = useRouter()
+const route = useRoute()
+
+const activeMenu = ref(route.path)
+const menuList = router.getRoutes().filter((route) => {
+  return route.meta.isShow
+})
 
 </script>
 
