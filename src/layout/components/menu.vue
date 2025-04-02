e39e7e199bb812a747014c542c995525f49aa675
johnnywwy <johnnywwy@gmail.com>
2025-04-02 10:10:43 +0800

    feat: 搜索页面完成

--- src/layout/components/menu.vue
+++ src/layout/components/menu.vue
@@ -3,9 +3,6 @@
     <el-menu-item v-for="item in menuList" :key="item.path" :index="item.path">
       {{ item.meta.title }}
     </el-menu-item>
-    <!-- <el-menu-item index="/user"> 用户列表 </el-menu-item>
-    <el-menu-item index="/role"> 角色列表 </el-menu-item>
-    <el-menu-item index="/auth"> 权限列表 </el-menu-item> -->
   </el-menu>
 </template>
 
