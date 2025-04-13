dca1541de3076facdd4330de2b47426f4ce47dfa
johnnywwy <johnnywwy@gmail.com>
2025-04-13 21:08:02 +0800

    feat: 封装 useComRef hooks

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="wrap-about">about</div>
+  <div class="wrap-about">
+    <ElForm ref="formRef"></ElForm>
+
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElForm } from 'element-plus';
+import { useComRef } from '@/hooks/useCompRef'
+const formRef = useComRef<typeof ElForm>()
+
+</script>
+
 <style lang="less">
 .wrap-about {
   color: @color;
